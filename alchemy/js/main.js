29d22d6991207b5062e621fb71f85b4b4807e3aa
Fesmaster <fessmk@gmail.com>
2021-02-09 21:04:02 -0500

    Sound added, among other tweaks.

--- alchemy/js/main.js
+++ alchemy/js/main.js
@@ -5,19 +5,15 @@ import "./phaser.js";
  */
 function UpdateProgressBarH(graphics, x, y, w, h, percent, ticks, color_f, color_t){
     if (percent > 1){percent = 1;}
-    graphics.clear();
+    graphics.fillStyle(0x666666);
+    graphics.fillRect(x-2, y-2, w+4, h+4);
     if (percent != 0){
         if (color_f == undefined){color_f = 0x00FF00;}
         graphics.fillStyle(color_f);
         graphics.fillRect(x, y, w*percent, h);
     }
-    if (percent != 1){
-        graphics.fillStyle(0x666666);
-        graphics.fillRect(x+w*percent, y, w - (w*percent), h);
-    }
-    
     if (ticks != undefined){
-        if (color_t == undefined){color_t = 0xFF0000;}
+        if (color_t == undefined){color_t = 0xDDDDDD;}
         var tickDist = 1.0 / ticks;
         graphics.lineStyle(1, color_t);
         for (var i = 0;i<ticks+1;i++){
@@ -39,18 +35,15 @@ function UpdateProgressRangeBarH(graphics, x, y, w, h, percent_low, percent_high
 
 function UpdateProgressBarV(graphics, x, y, w, h, percent, ticks, color_f, color_t){
     if (percent > 1){percent = 1;}
-    graphics.clear();
+    graphics.fillStyle(0x666666);
+    graphics.fillRect(x-2, y-2, w+4, h+4)
     if (percent != 0){
         if (color_f == undefined){color_f = 0x00FF00;}
         graphics.fillStyle(color_f);
         graphics.fillRect(x, y+ (h-(h*percent)), w, (h*percent));
     }
-    if (percent != 1){
-        graphics.fillStyle(0x666666);
-        graphics.fillRect(x, y, w, h-(h*percent))
-    }
     if (ticks != undefined){
-        if (color_t == undefined){color_t = 0xFF0000;}
+        if (color_t == undefined){color_t = 0xDDDDDD;}
         var tickDist = 1.0 / ticks;
         graphics.lineStyle(1, color_t);
         for (var i = 0;i<ticks+1;i++){
@@ -69,6 +62,7 @@ function lerp(a, b, alpha){
     return a*alpha + b * (1-alpha)
 }
 
+
 class AlchemistScene extends Phaser.Scene {
     
     constructor() {
@@ -77,22 +71,18 @@ class AlchemistScene extends Phaser.Scene {
         
         this.Buttons = [];
         this.ButtonItems = [];
+        this.reloadButton = null;
         
-        this.ProgressBars = [];
-        this.TargetBars = [];
+        this.BAR = null; //this draws ALL the progressbars
+        
         this.percents = [];
         this.percents_tv = [];
+        this.percentPerClick = 0.05;
         
         this.targets = [];
         this.target_dev = [];
-        for(var i=0;i<4;i++){
-            this.targets[i] = lerp(0.25, 0.75, Math.random());
-            this.target_dev[i] = lerp(0.05, 0.125, Math.random());
-        }
         this.barLableList = ["Color", "Smell", "Acidity", "Viscosity"];
         
-        
-        this.FillBar = null;
         this.FillPercent = 0.0;
         this.FillPercent_tv = 0.0;
         this.FillPerClick = 0.01;
@@ -120,7 +110,37 @@ class AlchemistScene extends Phaser.Scene {
             {img: "item_09", 0: -1, 1: -1, 2: 2, 3: 0},
             {img: "item_10", 0: 2, 1: 0, 2: -1, 3: -1},
             {img: "item_11", 0: -1, 1: 2, 2: 0, 3: -1},
-        ]
+        ];
+        
+        this.audio_on = false;
+        this.SoundSplash = null;
+        this.SoundOverflow = null;
+        
+        this.winText = null;
+        this.looseText = null;
+    }
+    
+    resetGame(){
+        //reset everythong
+        
+        for(var i=0;i<4;i++){
+            this.percents[i] = 0.0;
+            this.percents_tv[i] = 0.0;
+            this.targets[i] = lerp(0.25, 0.75, Math.random());
+            this.target_dev[i] = lerp(0.05, 0.125, Math.random());
+        }
+        this.FillPercent = 0;
+        this.FillPercent_tv = 0.0;
+        if (this.winText != null){
+            this.winText.destroy();
+        }
+        if (this.looseText != null){
+            this.looseText.destroy();
+        }
+        if (this.reloadButton != null){
+            this.reloadButton.setVisible(false);
+        }
+        this.inputEnabled = true;
     }
     
     setButtonAttrib(id){
@@ -142,37 +162,54 @@ class AlchemistScene extends Phaser.Scene {
     }
     
     preload() {
-        this.load.image("cauldron", "assets/Cauldron.png")
-        this.load.image("fluid", "assets/Cauldron_Fluid.png")
-        
+        this.load.image("cauldron", "assets/Cauldron.png");
+        this.load.image("fluid", "assets/Cauldron_Fluid.png");
+        this.load.image("reload", "assets/Reload.png");
         //load all the item images
         for(var i=0;i<this.Items.length;i++){
-            this.load.image(this.Items[i].img, "assets/"+this.Items[i].img+".png")
+            this.load.image(this.Items[i].img, "assets/"+this.Items[i].img+".png");
         }
+        
+        this.load.audio("splash", "assets/Splash.wav");
+        this.load.audio("overflow", "assets/Overflow.mp3");
     }
     
     buttonPress(index){
+        if (!this.audio_on){
+            this.sound.context.resume();
+            this.audio_on = true;
+        }
+        
         //disable input on win/loose
-        if (!this.inputEnabled){return;}
+        if (!this.inputEnabled){
+            if (index == -1){//-1 is the index of the reload button
+                this.resetGame();
+            }
+            return;
+        }else if (index == -1){
+            return; //the reload button was pressed 
+        }
         
         //get the item in the button, and modify the four bars to show it's changes
         //this.ButtonItems[index]
         for(var i = 0;i<4;i++){
-            this.percents_tv[i] += 0.05 * this.Items[this.ButtonItems[index]][i];
+            this.percents_tv[i] += this.percentPerClick * this.Items[this.ButtonItems[index]][i];
         }
-        
         
         //increase the fill on the fill percent bar
         this.FillPercent_tv += this.FillPerClick;
+        
+        //play sound
+        this.SoundSplash.play();
     }
     
     create() {
+        //reset the game
+        this.resetGame();
         
         //add the buttons
         for (var i = 0; i<4;i++){
             var b = this.add.image(125 + i*175, 475, this.Items[0].img);
-            //this.ButtonItems[i] = i;
-            //(125 + i*175, 475, 100, 100, 0x0077FF);
             b.setScale(3.125);
             b.setInteractive();
             b.buttonIndex = i;
@@ -188,23 +225,19 @@ class AlchemistScene extends Phaser.Scene {
             this.setButtonAttrib(i);
         }
         
+        
+        
+        
         //add the progressbars
+        this.BAR = this.add.graphics();
         for (var i = 0; i<4;i++){
-            this.percents[i] = 0.0;
-            this.percents_tv[i] = 0.0;
-            
             this.add.text(425, 20+80*i, this.barLableList[i], {color : '#0f0'})
-            
-            var b = this.add.graphics();
-            UpdateProgressBarH(b, 425, 40 + 80 * i, 335, 20, this.percents[i]);
-            this.ProgressBars[i] = b;
-            
-            var t = this.add.graphics();
-            UpdateProgressRangeBarH(t, 425, 30 + 80*i, 335, 40, 
-                                    this.targets[i] - (this.target_dev[i]/2),
-                                    this.targets[i] + (this.target_dev[i]/2),
-                                    0xFF0000
-                                   )
+            UpdateProgressBarH(this.BAR, 425, 40 + 80 * i, 335, 20, this.percents[i]);
+            UpdateProgressRangeBarH(this.BAR, 425, 30 + 80*i, 335, 40, 
+                this.targets[i] - (this.target_dev[i]/2),
+                this.targets[i] + (this.target_dev[i]/2),
+                0xFF0000
+            )
             
         }
         
@@ -215,20 +248,34 @@ class AlchemistScene extends Phaser.Scene {
         //(why? the cauldron has a hole to let the fluid be seen through, this grounds it well.)
         this.add.image(175, 175, 'cauldron');
         
-        //add ProgressBar next to it
-        this.FillBar = this.add.graphics();
-        UpdateProgressBarV(this.FillBar, 
-                           285, 50, 50, 250, 
-                           this.FillPercent, 
-                           8, 
-                           this.fluid_color, 0xDDDDDD);
-        //
+        UpdateProgressBarV(this.BAR, 
+            285, 50, 50, 250, 
+            this.FillPercent, 
+            8, 
+            this.fluid_color, 0xDDDDDD
+        );
         
+        this.SoundSplash = this.sound.add("splash");
+        this.SoundOverflow = this.sound.add("overflow");
         
-        
+        //reload button
+        //last, so drawn on top!
+        this.reloadButton = this.add.image(100, 100, "reload");
+        this.reloadButton.setInteractive();
+        this.reloadButton.buttonIndex = -1;
+        this.reloadButton.on("pointerover", function(){this.setScale(1.1);});
+        this.reloadButton.on("pointerout", function(){this.setScale(1);});
+        this.reloadButton.on("pointerup", function(){this.setScale(1.1);});
+        this.reloadButton.on("pointerdown", function(){
+            this.setScale(0.9);
+            this.scene.buttonPress(this.buttonIndex);
+        });
+        this.reloadButton.setVisible(false);
     }
     
     update() {
+        
+        this.BAR.clear(); //clear the graphics item
         
         //update the progressbars
         for (var i=0;i<4;i++){
@@ -237,7 +284,6 @@ class AlchemistScene extends Phaser.Scene {
             
             var diff = this.percents[i] - this.percents_tv[i]
             if (diff != 0){
-                
                 if (Math.abs(diff) < this.ProgressBarSpeed){
                     this.percents[i] = this.percents_tv[i];
                 }else{
@@ -247,11 +293,18 @@ class AlchemistScene extends Phaser.Scene {
                         this.percents[i] -= this.ProgressBarSpeed;
                     }
                 }
-                
                 if (this.percents[i] > 1){this.percents[i] = 1;}
                 if (this.percents[i] < 0){this.percents[i] = 0;}
-                UpdateProgressBarH(this.ProgressBars[i], 425, 40 + 80 * i, 335, 20, this.percents[i]);
             }
+            UpdateProgressBarH(this.BAR, 425, 40 + 80 * i, 335, 20, 
+                this.percents[i], 
+                1/this.percentPerClick
+            );
+            UpdateProgressRangeBarH(this.BAR, 425, 30 + 80*i, 335, 40, 
+                this.targets[i] - (this.target_dev[i]/2),
+                this.targets[i] + (this.target_dev[i]/2),
+                0xFF0000
+            )
         }
         
         //COLOR CHANGING!
@@ -277,30 +330,36 @@ class AlchemistScene extends Phaser.Scene {
             }
             if (this.FillPercent < 0){this.FillPercent = 0;}
         }
-        UpdateProgressBarV(this.FillBar, 285, 50, 50, 250, this.FillPercent, 8, this.fluid_color, 0xDDDDDD);
+        UpdateProgressBarV(this.BAR, 285, 50, 50, 250, this.FillPercent, 8, this.fluid_color);
         
-        
-        //check loose condition
-        if (this.FillPercent > 1){
-            this.FillPercent = 1;
-            this.add.text(35, 200, "YOU LOOSE!", {color: "#fff", fontSize: "128px"})
-            this.add.text(90, 325, "Container Overflow gets the best of all of us!", 
-                          {color: "#fff", fontSize: "24px"})
-            this.inputEnabled = false;
-        }
-        
-        //check for win condition
-        var win = true;
-        for (var i=0;i<4;i++){
-            if (!(CheckTargetDev(this.percents[i], this.targets[i], this.target_dev[i]))){
-                win = false;
-                break;
+        if (this.inputEnabled){
+            //check loose condition
+            if (this.FillPercent > 1){
+                this.FillPercent = 1;
+                this.winText = this.add.text(35, 200, "YOU LOOSE!", {color: "#fff", fontSize: "128px"})
+                this.looseText = this.add.text(90, 325, "Container Overflow gets the best of all of us!", 
+                        {color: "#fff", fontSize: "24px"})
+                this.SoundOverflow.play();
+                this.inputEnabled = false;
+                this.reloadButton.setVisible(true);
+            }
+            
+            //check for win condition
+            
+            var win = true;
+            for (var i=0;i<4;i++){
+                if (!(CheckTargetDev(this.percents[i], this.targets[i], this.target_dev[i]))){
+                    win = false;
+                    break;
+                }
+            }
+            if (win){
+                this.winText = this.add.text(100, 200, "YOU WIN!", {color: "#fff", fontSize: "128px"})
+                this.inputEnabled = false;
+                this.reloadButton.setVisible(true);
             }
         }
-        if (win){
-            this.add.text(100, 200, "YOU WIN!", {color: "#fff", fontSize: "128px"})
-            this.inputEnabled = false;
-        }
+        
         
     }
 }
